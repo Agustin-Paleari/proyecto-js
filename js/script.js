e28b8ca55477a6f3986721a07d6fc1b8c762af12
Agustin_Paleari <aguspaleari@gmail.com>
2021-12-29 15:52:10 -0300

    Primera entrega

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
-let resultado = 0;
+class Material{
+    constructor(tipo, cantidad, estado) {
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+        this.estado = estado;
+    }
+}
+alert("crearemos una lista del material adquirido ")
+alert("para ello agregaremos el tipo de material, cantidad y el estado del mismo")
+//saludo es hasta aca
 
-function sumar(primerNumero, segundoNumero) {
-    resultado = primerNumero + segundoNumero;
+const materialAdquirido = []; // DECLARO EL ARRAY PARA GUARDAR DATOS
+
+//Funcion para solicitar el agregado de datos
+function continuar() {
+    const seguir = confirm("continuar agregando material?")
+    return seguir;
 }
 
-function mostrar(mensaje) {
-    console.log(mensaje);
+// FUNCION PARA EL INGRESO DE LOS DATOS
+function agregarMaterial() {
+    const tipo = prompt("ingrese el tipo de material, \n EJ: mosqueton  ")
+    const cantidad = prompt("ingrese la cantidad que posee del mismo, \n EJ: 5")
+    const estado = prompt("ingrese el estado del mismo, \n EJ: bueno/malo")
+    const ingresoUsuario = new Material(tipo, cantidad, estado);
+    materialAdquirido.push(ingresoUsuario)
+    console.log(materialAdquirido)
 }
-let uno =parseInt(prompt("ingrese un numero"))
-let dos = parseInt(prompt("ingrese otro"))
+// INICIO
 
-sumar(uno ,dos);            
-mostrar(resultado); 
+function init() {
+    do {
+        agregarMaterial()
+        
+    } while (continuar() == true);
+    
+}
+init()
+
+
